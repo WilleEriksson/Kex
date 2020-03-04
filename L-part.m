379036948b304e4ddfc1e089c1b0c84974bf981a
WilleEriksson <37901079+WilleEriksson@users.noreply.github.com>
2020-03-04 17:36:01 +0100

    Add matlab code

---
+++ L-part.m
@@ -0,0 +1,28 @@
+tic
+N = 4;
+M = 5;
+eta = 0.1;%1e-1;%1e-2;%1e-3;%*1e1;
+epsilon1 = 0.01;%0.1;%0.01;%0.1*4/N^(3/2);
+
+z = zeros(N,1);
+e = ones(N,1);
+E = ones(N,N)/N;
+S = find(eye(N,N));
+
+
+X = randi([0, 1], [N,M]);
+Y = X;
+
+
+cvx_begin % quiet
+    variable L(N,N) symmetric
+    W = L-diag(diag(L));
+    minimize( trace(Y'*L*Y) + eta*norm(L,'fro'));
+    subject to
+        trace(L) == N;
+        W(:) <= 0;
+        L*e == z;
+cvx_end
+
+clear
+toc

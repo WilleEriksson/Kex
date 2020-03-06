5b0fb72af0838132b9a9c035a370bf88927fbc9c
Wille Eriksson <willee@kth.se>
2020-03-06 16:44:47 +0100

    Add Y-part

--- L-part.m
+++ L-part.m
@@ -13,16 +13,22 @@ S = find(eye(N,N));
 X = randi([0, 1], [N,M]);
 Y = X;
 
+for iter=1:5
 
-cvx_begin % quiet
-    variable L(N,N) symmetric
-    W = L-diag(diag(L));
-    minimize( trace(Y'*L*Y) + eta*norm(L,'fro'));
-    subject to
-        trace(L) == N;
-        W(:) <= 0;
-        L*e == z;
-cvx_end
+    cvx_begin % quiet
+        variable L(N,N) symmetric
+        W = L-diag(diag(L));
+        minimize( trace(Y'*L*Y) + eta*L.^2;
+        subject to
+            trace(L) == N;
+            W(:) <= 0;
+            L*e == z;
+    cvx_end
+
+
+    Y = (eye(N)+L) \ X
+
+end
 
 clear
 toc

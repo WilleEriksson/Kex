12357bbe951052dd38232a79e850b2246d1d791a
Wille Eriksson <willeeriksson@n187-p45.eduroam.kth.se>
2020-03-10 12:27:36 +0100

    Make faster vroom vroom

--- flz-seg.py
+++ flz-seg.py
@@ -7,17 +7,6 @@ def nseg(inputlist):
     # Determines the number of segments
     return max(max(inputlist.max(1)),max(inputlist.max(0)))
 
-def getimg(loaded,w,h):
-    # Converts loaded image into np-array of 3-tuples
-
-    img = np.zeros((h, w, 3))
-
-    for j in range(h):
-        for i in range(w):
-            img[j][i]=loaded.getpixel((i,j))
-
-    return img
-
 def getcolors(seg):
     # Randomly generates unqiue colors for every segment
     colors = []
@@ -45,14 +34,13 @@ def main(image,mode='show'):
     Performs a Felzenswalb segmenation on an input image
     and returns an image where every segment is uniquely colored.
     '''
-    print("Preparing...")
 
     loaded = Image.open(image) # Using library PIL
 
     loaded = loaded.convert(mode='RGB')
     w,h = loaded.size
 
-    img = getimg(loaded,w,h)
+    img = np.array(loaded)
 
     print("Segmenting...")
     seg = sgm.felzenszwalb(img,1,0.8,1000) # Using the sci-kit image library
